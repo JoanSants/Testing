37e0fb78678c790efd7d7fed43c51807672dd62a
Joan Santos <joansants07@gmail.com>
2020-03-26 20:49:42 -0300

    Simulate click on button component

--- src/component/button/index.test.js
+++ src/component/button/index.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 
 import Button from '../../../src/component/button';
 import { findByTestAttr, checkProps } from '../../../utils';
@@ -15,14 +16,22 @@ describe('Button Component', () => {
 
 	describe('Renders', () => {
 		let wrapper;
-		beforeEach(() => wrapper = setup({ 
-			content: 'content', 
-			onClick: () => {}
-		}));
+		let mockFunc;
+		beforeEach(() => {
+			mockFunc = jest.fn();
+			wrapper = setup({ content: 'content', onClick: mockFunc });
+		});
 
 		it('Should render a button', () => {	
 			const button = findByTestAttr(wrapper, 'Button');
 			expect(button.length).toBe(1);
 		});
-	});
+
+		it('Should emit callback on click event', () => {
+			const button = findByTestAttr(wrapper, 'Button');
+			button.simulate('click');
+			const callback = mockFunc.mock.calls.length;
+			expect(callback).toBe(1);
+		});
+	});	
 });
